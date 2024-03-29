3b3d2c3cf272e01031c6a06e64c834595e5ef418
Miiladev <116461642+Miiladev@users.noreply.github.com>
2024-03-29 14:37:19 -0500

    Delete frontend/js directory

--- frontend/js/ObtenerTranscripcion.js
+++
@@ -1,9 +0,0 @@
-function obtenerTranscripcion(event) {
-    let transcripcion = "";
-    for (let i = event.resultIndex; i < event.results.length; i++) {
-        transcripcion += event.results[i][0].transcript.trim();
-    }
-    return transcripcion;
-}
-
-export { obtenerTranscripcion };
